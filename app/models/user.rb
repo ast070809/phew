830f80c4b6a0e0d206b1a6c4c3faad3db7c5d7f2
root <root@Mark-II.(none)>
2014-03-23 01:13:45 +0530

    add username for login

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,10 +25,28 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  
+	validates :username,
+	  :uniqueness => {
+	    :case_sensitive => false
+	  }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  
+  attr_accessor :login
 
   has_many :posts
   has_and_belongs_to_many :tribes
   acts_as_tagger
+
+
+	def self.find_first_by_auth_conditions(warden_conditions)
+	  conditions = warden_conditions.dup
+	  if login = conditions.delete(:login)
+	    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+	  else
+	    where(conditions).first
+	  end
+	end
  end
