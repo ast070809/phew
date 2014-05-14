a487033f64357648bfa186fb11f5b705e6489266
root <root@Mark-II.(none)>
2014-05-14 21:40:34 +0530

    new about us

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  #has_mobile_fu
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  #before_filter :set_request_format
 
   protected
 
@@ -11,6 +13,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
+  end
+
+
+  def set_request_format
+    request.format = :mobile if is_mobile_device?
   end
 
 end
