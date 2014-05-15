769a893e79a371457ac4cff4cae37ddc0fe1bc34
root <root@Mark-II.(none)>
2014-05-16 00:02:42 +0530

    mobile sign in and sign up pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  #has_mobile_fu
+  has_mobile_fu
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
-  #before_filter :set_request_format
+  before_filter :set_request_format
 
   protected
 
