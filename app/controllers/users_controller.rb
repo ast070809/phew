37c6552bc30ebb9c8461cdf85aaee8c3a4bc924f
root <root@Mark-II.(none)>
2014-05-21 11:20:38 +0530

    bug fix in pagination of links and comments in my account page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
         @number_posts = @posts.count
         @posts = @posts.order('created_at desc').page(params[:page]).per(links_per_page)
 		
-		@comments = Comment.find_comments_by_user(@user).page(params[:page]).per(links_per_page)
+		@comments = Comment.find_comments_by_user(@user).page(params[:pagina]).per(links_per_page)
 		@number_comments = Comment.total_comments_of_user(@user)
 	end
 end
