aa3065dc428278cac493596860f68b215d6eb7e3
root <root@Mark-II.(none)>
2014-07-07 12:14:25 +0530

    testing amazon cloudfront

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Phew::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -109,5 +109,8 @@ Phew::Application.configure do
   require 'net/http'
   GA.tracker = "UA-50235584-1"
 
-  
+  # set caching for cloudfront
+  config.static_cache_control = "public, max-age=31536000"
+  # host name for the CDN
+  config.action_controller.asset_host = ENV['CDN_HOSTNAME']
 end
